09ad6b61435faaa910f926bd1e5f67d38a17dee8
daryckont <darybnss@gmail.com>
2026-01-01 19:28:08 -0500

    remove local variable info in take method
    Use merge method instead of merge!

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -72,13 +72,11 @@ class Hangman
     self.user_guess = guess
     search_in_secret_word
 
-    info = inform_user
-
-    if info[:ended]
-      info.merge!(won: guessed?, secret: secret_word)
+    if inform_user[:ended]
+      inform_user.merge(won: guessed?, secret: secret_word)
+    else
+      inform_user
     end
-
-    info
   end
 
 end
